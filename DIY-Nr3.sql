3d5f53a4bd569a7a0934e1322e5a2372c7d71277
Lukas <157211264+LukasKnt@users.noreply.github.com>
2024-11-22 14:05:56 +0100

    sql nr 3 added code

--- DIY-Nr3.sql
+++ DIY-Nr3.sql
@@ -1,1 +1,12 @@
 -- Aufgabe 3
+CREATE INDEX idx_hoeren_matrnr ON hoeren (MatrNr);
+CREATE INDEX idx_hoeren_vorlnr ON hoeren (VorlNr);
+CREATE INDEX idx_studenten_name ON Studenten (Name);
+
+EXPLAIN ANALYSE
+SELECT DISTINCT s.Name
+FROM Studenten s
+JOIN hoeren h1 ON s.MatrNr = h1.MatrNr
+JOIN hoeren h2 ON h1.VorlNr = h2.VorlNr
+JOIN Studenten schopenhauer ON h2.MatrNr = schopenhauer.MatrNr
+WHERE schopenhauer.Name = 'Schopenhauer' AND s.Name != 'Schopenhauer';
